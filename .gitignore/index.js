3946bc0d09df3dd971b83f07729433629d7cf9ee
yanubix <75914480+yanubix@users.noreply.github.com>
2020-12-13 00:11:39 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,9 @@
+Discord = require("discord.js");
+
+const Client = new Discord.Client;
+
+Client.on("ready", () => {
+     console.log("bot opérationnel");
+});
+
+    Client.login("Nzg3NDI5MjEzMTQ5NzI0NzAy.X9U0mg.Yc70rY58SGEuEg-WnZcs_g44jOY");
